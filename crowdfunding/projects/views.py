170e88fd756f6586d25c2b7c55f9d662d9f5f098
nirmaljitkaur <nirmalkahlon8@gmail.com>
2023-08-01 20:10:52 +1000

    Permission, authentication done

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -6,9 +6,12 @@ from rest_framework.response import Response
 from .models import Project, Pledge
 from .serializers import ProjectSerializer, PledgeSerializer, ProjectDetailSerializer
 from django.http import Http404
-from rest_framework import status
+from rest_framework import status, permissions
+from .permissions import IsOwnerOrReadOnly
+
 
 class ProjectList(APIView):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     def get(self, request):
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
@@ -28,7 +31,10 @@ class ProjectList(APIView):
 class ProjectDetail(APIView):
     def get_object(self, pk):
         try:
-            return Project.objects.get(pk=pk)
+            #return Project.objects.get(pk=pk)
+            project = Project.objects.get(pk=pk)
+            self.check_object_permissions(self.request, project)
+            return project
         except Project.DoesNotExist:       
             raise Http404
         
@@ -37,6 +43,19 @@ class ProjectDetail(APIView):
         project = self.get_object(pk)
         serializer = ProjectDetailSerializer(project)
         return Response(serializer.data)
+    
+    def put(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectDetailSerializer(
+            instance=project,
+            data=request.data,
+            partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+    
+    def fname(arg):
+        pass
 class PledgeList(APIView):
     def get(self,request):
         pledges = Pledge.objects.all()
